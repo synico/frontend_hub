a8d7044546eec2b27868fb454e53b0cd9efe543e
Nick Liu <synico@qq.com>
2020-08-03 17:58:25 +0800

    20200803

--- react_demo/src/index.js
+++ react_demo/src/index.js
@@ -1,122 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import app from './react_ctx';
+import {ThemeContext, themes} from './context/theme-context';
+import ThemedButton from './context/themed-button';
 
-// import { add } from './math.js';
-
-function FancyBoarder(props) {
+function Toolbar(props) {
   return (
-    <div className={'FancyBoarder FancyBoarder-' + props.color}>
-      {props.children}
-    </div>
+    <ThemedButton onClick={props.changeTheme}>
+      Change Theme
+    </ThemedButton>
   );
 }
 
-function WelcomeDialog() {
-  import("./math").then(math => {
-    console.log(math.add(10, 12));
-  });
-  return (
-    <FancyBoarder color="blue">
-      <h1 className="Dialog-title">
-        Welcome
-      </h1>
-      <p className="Dialog-message">
-        Thank you for visiting our spacecraft!
-      </p>
-    </FancyBoarder>
-  );
-}
-
-////////////////////////////////////////////////////
-
-class NameForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {value: 'coconut'};
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({value: event.target.value});
-  }
-
-  handleSubmit(event) {
-    alert('name has been submitted: ' + this.state.value);
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <form onSubmit={this.handleSubmit}>
-        <label>Name: 
-        <select value={this.state.value} onChange={this.handleChange}>
-          <option value="grapefruit">葡萄柚</option>
-          <option value="lime">酸橙</option>
-          <option value="coconut">椰子</option>
-          <option value="mango">芒果</option>
-        </select>
-        </label>
-        <input type="submit" value="SUBMIT" />
-      </form>
-    );
-  }
-
-}
-
-class Reservation extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      isGoing: true,
-      numberOfGuests: 2
+      theme: themes.light,
     };
 
-    this.handleInputChange = this.handleInputChange.bind(this);
-  }
-
-  handleInputChange(event) {
-    const target = event.target;
-    const value = target.name === 'isGoing' ? target.checked : target.value;
-    const name = target.name;
-
-    this.setState({
-      [name]: value
-    });
+    this.toggleTheme = () => {
+      this.setState(state => ({
+        theme:
+          state.theme === themes.dark ? themes.light : themes.dark,
+      }));
+    };
   }
 
   render() {
     return (
-      <form>
-        <label>
-          参与:
-          <input
-            name="isGoing"
-            type="checkbox"
-            checked={this.state.isGoing}
-            onChange={this.handleInputChange} />
-        </label>
-        <br />
-        <label>
-          来宾人数：
-          <input
-            name="numberOfGuests"
-            type="number"
-            value={this.state.numberOfGuests}
-            onChange={this.handleInputChange} />
-        </label>
-      </form>
+      <div>
+        <ThemeContext.Provider value={this.state.theme}>
+          <Toolbar changeTheme={this.toggleTheme} />
+        </ThemeContext.Provider>
+        <ThemedButton />
+        </div>
     );
   }
 }
 
-ReactDOM.render(
-  // <NameForm />,
-  // <Reservation />,
-  // <WelcomeDialog />,
-  <app.App />,
-  document.getElementById('root')
-);
+ReactDOM.render(<App/>, document.getElementById('root'));
