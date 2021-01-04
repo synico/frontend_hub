ff02b179a1507853f63b4438b6a94f6244415435
Nick <synico@qq.com>
2021-01-04 14:47:16 +0800

    add react router demo

---
+++ react_demo/nesting/src/example.js
@@ -0,0 +1,90 @@
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams,
+  useRouteMatch
+} from 'react-router-dom';
+
+export default function NestingExample() {
+  return (
+    <Router>
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/topics">Topics</Link>
+          </li>
+        </ul>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/topics">
+            <Topics />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+    </div>
+  );
+}
+
+function Topics() {
+  let { path, url } = useRouteMatch();
+
+  return (
+    <div>
+      <h2>Topics</h2>
+      <ul>
+        <li>
+          <Link to={`${url}/rendering`}>Rendering with React</Link>
+        </li>
+        <li>
+          <Link to={`${url}/components`}>Components</Link>
+        </li>
+        <li>
+          <Link to={`${url}/v1/props-v-state`}>Props v. State</Link>
+        </li>
+      </ul>
+
+      <Switch>
+        <Route exact path={path}>
+          <h3>Please select a topic.</h3>
+        </Route>
+        <Route path={`${path}/:version/:topicId`}>
+          <Topic />
+        </Route>
+        <Route path={`${path}/:topicId`}>
+          <Topic />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+function Topic() {
+  let obj = useParams();
+  let { topicId, version } = useParams();
+
+  return (
+    <div>
+      <h3>version: {version}</h3>
+      <h3>topic: {topicId}</h3>
+    </div>
+  );
+}
--- react_demo/src/RefForward.js
+++
@@ -1,8 +0,0 @@
-const FancyButton = React.forwardRef((props, ref) => (
-    <button ref={ref} className="FancyButton">
-        {props.children}
-    </button>
-));
-
-const ref = React.createRef();
-<FancyButton ref={ref}>Click me!</FancyButton>
