c9406b14840cbbc5466ef16c0f794f6960bba00e
Nick <synico@qq.com>
2021-03-10 18:01:15 +0800

    function and generic

--- ts3_demo/src/genericType/genType1.ts
+++ ts3_demo/src/genericType/genType1.ts
@@ -17,3 +17,38 @@ console.log("number: " + myIdentity1(11));
 // let myGenericNumber = new GenericNumber<number>();
 // myGenericNumber.zeroValue = 0;
 // myGenericNumber.add = function(x, y) { return x + y; };
+
+
+type Filter1 = {
+    <T>(array: T[], f: (item: T) => boolean): T[]
+}
+let filter1: Filter1 = (array, f) => array.filter(f);
+let filter2: Filter1 = (array, f) => {
+    let rst = array.slice(0, 0);
+    for (let i = 0; i < array.length; i++) {
+        if (f(array[i])) {
+            rst.push(array[i]);
+        }
+    }
+    return rst;
+}
+let rst2 = filter2([1, 2, 3, 4], _ => _ > 2);
+console.log("rst2: " + rst2);
+
+type Filter3<T> = {
+    (array: T[], f: (item: T) => boolean): T[]
+}
+let filter3: Filter3<number> = (array, f) => array.filter(f);
+let rst3 = filter3([1, 2, 3, 4], _=>_> 2);
+console.log("rst3: " + rst3);
+
+type Filter4 = <T>(array: T[], f: (item: T) => boolean) => T[];
+
+function filter5<T>(array: T[], f: (item: T) => boolean): T[] {
+    let rst = array;
+    return rst;
+}
+
+let set = new Set;
+set.add(1).add(2);
+set.has(2);
--- ts3_demo/src/modules/test.ts
+++ ts3_demo/src/modules/test.ts
@@ -33,8 +33,9 @@ type Filter = {
   <T>(array: T[], f: (item: T) => boolean): T[]
 }
 
-let filter: Filter = (array, f) => array.filter(f);
-
+let filter: Filter = (array, f) => {
+  return array.filter(f);
+}
 let rst = filter([1, 2, 3], _=>_>2);
 console.log('rst: ' + rst);
 
@@ -46,3 +47,53 @@ let names = [
 
 let rstString = filter(names, _=>_.firstName.startsWith('b'));
 console.log('rstString: ' + rstString.length);
+
+function map<T, U>(array: T[], f: (item: T) => U): U[] {
+  let result: Array<U> = [];
+  for (let i = 0; i < array.length; i++) {
+    result[i] = f(array[i]);
+  }
+  return result;
+}
+
+
+type Greet = (name?: string) => string
+
+let theGreet: Greet = (name = 'God') => {
+  return "Hello, " + name;
+}
+
+console.log(theGreet());
+
+type Greeting = {
+  (name?: string): string
+}
+
+let theGreeting: Greeting = (name) => {
+  return "Hello, greeting " + name;
+}
+console.log(theGreeting('Nick'));
+
+interface MyGreet {
+  (name?: string): string
+}
+
+let theMyGreet: MyGreet = (name) => {
+  return "Hello, my greet " + name;
+}
+console.log(theMyGreet('Jack'));
+
+type WarnUser = {
+  (warning: string): void
+  wasCalled: boolean
+}
+
+// let warnUser: WarnUser = (warning: string) => {
+//   if (warnUser.wasCalled) {
+//     return;
+//   }
+//   warnUser.wasCalled = true
+//   alert(warning);
+// }
+
+// warnUser.wasCalled = false;
