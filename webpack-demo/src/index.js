eded7163c491de5bcc7666e97c0effec2a5a1c13
nick@p50 <synico@qq.com>
2023-08-07 22:46:17 +0800

    feat: init webpack demo
    Signed-off-by: nick@p50 <synico@qq.com>

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,27 +1,10 @@
 import _ from 'lodash';
-import './style.css';
-import printMe from './print.js';
 
 function component() {
-  var element = document.createElement('div');
-  var btn = document.createElement('button');
+  const element = document.createElement('div');
 
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-  btn.innerHTML = 'CLick me and check the console!';
-  btn.onclick = printMe;
-
-  element.appendChild(btn);
-
   return element;
 }
 
 document.body.appendChild(component());
-
-if (module.hot) {
-  module.hot.accept('./print.js', function() {
-    console.log('Accepting the updated printMe module!');
-    printMe();
-  })
-}
